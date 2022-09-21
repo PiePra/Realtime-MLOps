7af95ec50ac4fbf29466e546a9c7eff0df2ae757
PiePra <pierre.prange@gmail.com>
2022-09-21 16:43:49 +0200

    Update the data-service.

--- demonstration/apps/data-service/app.py
+++ demonstration/apps/data-service/app.py
@@ -5,21 +5,27 @@ from flask import Flask
 from datetime import datetime
 
 app = Flask(__name__)
+attributes = {
+    "type": "cluster.local.crypto_price",
+    "source": "https://cluster.local/crypto_price",
+}
 
 @app.route("/btc")
 def get_btc():
-    attributes = {
-    "type": "com.example.sampletype1",
-    "source": "https://example.com/event-producer",
-    }
     r = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
-    print("test")
     data = {"price": float(r.json()['price']), "timestamp": datetime.now().timestamp(), "symbol": "BTC/USD"}
-    event = CloudEvent(attributes, data)
-    headers, body = to_structured(event)
-    requests.post("http://kafka-broker-ingress.knative-eventing.svc.cluster.local/default/btc", data=body, headers=headers)
     return "success"
 
+@app.route("/eth")
+def get_eth():
+    r = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT')
+    data = {"price": float(r.json()['price']), "timestamp": datetime.now().timestamp(), "symbol": "ETH/USD"}
+    return "success"
+
+def post_event(data):
+    event = CloudEvent(attributes, data)
+    headers, body = to_structured(event)
+    requests.post("http://kafka-broker-ingress.knative-eventing.svc.cluster.local/default/crypto", data=body, headers=headers)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=5000)
---
+++ demonstration/deployments/data-pipeline/README.md
@@ -0,0 +1,5 @@
+# Data-Pipeline
+
+The manifest is generated using 
+`waxctl dataflow deploy dataflow.py -n feast --dry-run`
+then remove the namespace and add the feast feature_store.yaml as configmap
