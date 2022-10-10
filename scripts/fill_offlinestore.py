ec5cfa1b09b2a19c8cb676dad0a43d790feb0d3f
PiePra <pierre.prange@gmail.com>
2022-10-10 18:44:41 +0200

    Switched to xgb regressor.

--- scripts/fill_offlinestore.py
+++ scripts/fill_offlinestore.py
@@ -2,12 +2,11 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-df = pd.read_csv('./data/BTC-2021min.csv')
+df = pd.read_csv('./data/BTCUSD_5.csv', header = None, names = ["unix", "open", "high", "low", "close", "volume", "trades"])
 df["unix"] = df["unix"].astype('datetime64[s]')
-df["date"] = df["date"].astype('datetime64[s]')
 df["timestamp"] = df["unix"]
 df["timestamp_created"] = df["unix"]
-df.drop(["unix", "date", "Volume BTC", "Volume USD"], inplace=True, axis=1)
+df.drop(["unix", "volume", "trades"], inplace=True, axis=1)
 
 
 engine = create_engine('postgresql://feast:feast@offline-store-postgresql.feast.svc.cluster.local:5432/feast')
