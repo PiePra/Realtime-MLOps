66077ddc34e95842f8ab68fdc6609c907bc4205e
PiePra <pierre.prange@gmail.com>
2022-10-11 16:40:27 +0200

    Deleted old file.

--- scripts/fill_offlinestore.py
+++ scripts/fill_offlinestore.py
@@ -6,6 +6,7 @@ df = pd.read_csv('./data/BTCUSD_5.csv', header = None, names = ["unix", "open", 
 df["unix"] = df["unix"].astype('datetime64[s]')
 df["timestamp"] = df["unix"]
 df["timestamp_created"] = df["unix"]
+df["symbol"] = "BTC/USD"
 df.drop(["unix", "volume", "trades"], inplace=True, axis=1)
 
 
