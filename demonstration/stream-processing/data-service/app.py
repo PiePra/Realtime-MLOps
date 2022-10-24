71e4eca7a95a795642f1741a3b11c8b0deaa04df
PiePra <pierre.prange@gmail.com>
2022-10-24 14:58:36 +0200

    Refactored data service.

--- demonstration/stream-processing/data-service/app.py
+++ demonstration/stream-processing/data-service/app.py
@@ -1,32 +1,55 @@
-import requests
-from cloudevents.http import CloudEvent
-from cloudevents.conversion import to_structured
-from datetime import datetime
 import time
-import logging
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Dict
 
-attributes = {
-    "type": "cluster.local.crypto_price",
-    "source": "https://cluster.local/crypto_price",
-}
+import requests
+from cloudevents.conversion import to_structured
+from cloudevents.http import CloudEvent
 
-def get_ticker(ticker):
-    try:
-        
-        r = requests.get(f'https://api.binance.com/api/v3/ticker/price?symbol={ticker.replace("/", "") + "T"}')
-        data = {"price": float(r.json()['price']), "timestamp": datetime.utcnow().timestamp(), "symbol": ticker}
-        try:
-            event = CloudEvent(attributes, data)
-            headers, body = to_structured(event)
-            requests.post("http://kafka-broker-ingress.knative-eventing.svc.cluster.local/default/crypto", data=body, headers=headers)
-            logging.info(f"created {event}")
-        except:
-            logging.error("broker not reachable")
-    except:
-        logging.error("api not reachable")
+#API Config
+API_URL="https://api.binance.com/api/v3/ticker/price"
+API_QUERY="symbol"
 
-if __name__ == "__main__":
+#CloudEvent Attributes
+CE_ATTR_TYPE="cluster.local.crypto_price"
+CE_ATTR_SOURCE="https://cluster.local/crypto_price"
+
+#Knative Attributes
+KNATIVE_BROKER_INGRESS="http://kafka-broker-ingress.knative-eventing.svc.cluster.local/default/crypto"
+
+def get_cloudevent(data: Dict) -> CloudEvent:
+    """Create a CloudEvent from a dict"""
+    attributes = {
+        "type": CE_ATTR_TYPE,
+        "source": CE_ATTR_SOURCE,}
+    return CloudEvent(attributes, data)
+
+def post_event(event: CloudEvent) -> None:
+    """Post a CloudEvent to the Broker Ingress"""
+    headers, body = to_structured(event)
+    requests.post(KNATIVE_BROKER_INGRESS, data=body, headers=headers)
+
+def format_ticker(ticker: str) -> str:
+    """Format Ticker according to Binance API"""
+    return ticker.replace("/","") + "T"
+
+def get_ticker(ticker: str) -> None:
+    """Query API to get price data"""
+    formatted_ticker = format_ticker(ticker)
+    r = requests.get(f'{API_URL}?{API_QUERY}={formatted_ticker}')
+    if r.status_code != 200:
+        return
+    price = float(r.json()['price'])
+    price_data = {"price": price, "timestamp": datetime.utcnow().timestamp(), "symbol": ticker}
+    event = get_cloudevent(price_data)
+    post_event(event)
+
+def run() -> None:
     while True:
         get_ticker("ETH/USD")
         get_ticker("BTC/USD")
         time.sleep(60)
+
+if __name__ == "__main__":
+    run()
